f3c3ee7d8c603b4c6d67b224ebde2637cb8984a2
James <jspielman52@gmail.com>
2024-07-06 16:25:50 -0400

    My E-portfolio (Without Projects).

---
+++ index.js
@@ -0,0 +1,68 @@
+// template_696fs2a
+// service_ggsmbwp
+// tCujToTqJsIJ-Oxnk
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 30;
+
+
+
+function contact(event){
+
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading")
+    const success = document.querySelector(".modal__overlay--success")
+    loading.classList += " modal__overlay--visible";
+
+    emailjs
+        .sendForm(
+            "service_ggsmbwp",
+            "template_696fs2a",
+            event.target,
+            "tCujToTqJsIJ-Oxnk"
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert("The email service is temporarily unavailable. Please contact me directly on jspielman52@gmail.com")
+        })
+
+
+    }
+
+
+function toggleModal(){
+    
+    if(isModalOpen){
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open"
+}
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if(contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+   
+
+}
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for(let i = 0; i < shapes.length; i++){
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1: 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
